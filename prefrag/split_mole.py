1370e1f388c809682c9693fec69922f3577b1d86
ansatz <hermit10032@163.com>
2022-04-30 19:13:36 +0800

    fix bugs

--- prefrag/split_mole.py
+++ prefrag/split_mole.py
@@ -86,6 +86,10 @@ def get_linkage_hydrogen_num(mol,atomidx):
         #print("get your h on this atom %s"% atomidx)
     return hs_num
 
+def get_atom_symbol(mol,idx):
+    atom =mol.GetAtomWithIdx(idx)
+    symbol = atom.GetSymbol()
+    return symbol
 
 def get_frag_atomindex_pro(smi):
     '''
@@ -97,12 +101,13 @@ def get_frag_atomindex_pro(smi):
     mol = pre_mole(smi)
     frag_index = get_orign_index(smi)
     unsaturated_atom ='0'
-    atomids =[]
+
 
     atoms =[]
     who = []
     for i in frag_index:
         atomlists =[]
+        atomids =[]
         frag1, frag2, mol_1, mol_2 = pre_frag(int(i))
         if '[H]'  not in [frag1,frag2]:
             
@@ -121,6 +126,8 @@ def get_frag_atomindex_pro(smi):
             
             else :
                 print("nothing match")
+                with open("not_natching.log","a+") as f:
+                    f.write("%s,%s,\n"% (inf,i))
             for atom in atomids:
                 atomlists.append(atom)
 
@@ -130,71 +137,124 @@ def get_frag_atomindex_pro(smi):
                 for H in addHs:
                     atomlists.append(H)
         else :
+            print("fuck h")
             if frag1 == '[H]':
                 use = 2
                 mol_H =Chem.MolFromSmiles(frag2)
-                
+                is_match =mol.HasSubstructMatch(mol_2)
             else :
                 use = 1
                 mol_H =Chem.MolFromSmiles(frag1)
+                is_match =mol.HasSubstructMatch(mol_1)
+
+            saturated_atoms=[]
+            if is_match :
+               if use == 1 :
+                   atomids = mol.GetSubstructMatch(mol_1)
+               else :
+                   atomids = mol.GetSubstructMatch(mol_2)
+
+               for atom in atomids:
+                    atomlists.append(atom)
+                    saturated_atoms.append(atom)
+                    
+                # 添加骨架原子in mole进去
+            else :
+                print("nothing match")
+                with open("not_natching.log","a+") as f:
+                   f.write("%s,%s,\n"% (inf,i))
+               
+                
+                for atom in range(mol_H.GetNumAtoms()):
+                    atomlists.append(atom)
+                    saturated_atoms.append(atom)  
+                    atomids.append(atom)
+                    print("aaa")
             #if  not h use it to fetch
             # can't match but we can find unsaturated atom
-            atomids =[]
-            saturated_atoms =[]
-            for atom in range(mol_H.GetNumAtoms()):
-                atomids.append(atom)
-                atomlists.append(atom)
-                saturated_atoms.append(atom)
-            # just do it
 
+
+            #print("mol_H.GetNumAtoms()",mol_H.GetNumAtoms())
+
+
+
+
+            print(atomids,atomlists)
+            # just do it
+            print("mol_H.GetNumAtoms()",mol_H.GetNumAtoms())
             for j in range(mol_H.GetNumAtoms()):
                 # try each atom but H
-                print(i,j,use)
+
+
                 atom =mol_H.GetAtomWithIdx(j)
                 # pick atom
                 bonds = atom.GetTotalValence()
                 symbol = atom.GetSymbol()
                 # infomation of this atom
 
-                
                 if symbol == 'C' and bonds == 3:
                     unsaturated_atom = atomids[j]
                     # if  then  mark it 
+                    print("i,j,use",i,j,use)
+                    print("BBS",bonds,symbol,j)
+                    print("it is C",j)
                     saturated_atoms.remove(unsaturated_atom)
                     break
                 if symbol == 'N' and bonds == 2:
                     unsaturated_atom = atomids[j]
+                    print("i,j,use",i,j,use)
+                    print("BBS",bonds,symbol,j)
+                    print("it is N")
                     saturated_atoms.remove(unsaturated_atom)
                     break
                 if symbol == 'O' and bonds == 1:
                     unsaturated_atom = atomids[j]
+                    print("i,j,use",i,j,use)
+                    print("BBS",bonds,symbol,j)
+                    print("it is O")
                     saturated_atoms.remove(unsaturated_atom)
                     break
             #index of frags --mapping entire molecule's index 
-            ua_mol = get_linkage_hydrogen_num(mol, unsaturated_atom)
-            ua_mol_H = get_linkage_hydrogen_num(mol_H, unsaturated_atom)
-            print("unsaturated_atom",unsaturated_atom)
-            if ua_mol == ua_mol_H:
-                atom_sametype = []
-                for atomidx in saturated_atoms:
-                    atom =mol_H.GetAtomWithIdx(atomidx)
-                    if atom.GetSymbol() == symbol:
-                        atom_sametype.append(atomidx)
-                    # get same type atoms
+            if is_match: 
+                continue
+            else :
+                ua_mol = get_linkage_hydrogen_num(mol, unsaturated_atom)
+                ua_mol_H = get_linkage_hydrogen_num(mol_H, unsaturated_atom)
+                ua_mol_s = get_atom_symbol(mol,unsaturated_atom)
+                ua_mol_H_s = get_atom_symbol(mol_H,unsaturated_atom)
+                print("unsaturated_atom",unsaturated_atom,ua_mol,ua_mol_H,ua_mol_s,ua_mol_H_s,frag2)
+            
+                if (ua_mol == ua_mol_H) or (ua_mol_s !=ua_mol_H_s) :
+                   atom_sametype = []
+                   for atomidx in saturated_atoms:
+                      s_atom_symbol=get_atom_symbol(mol,atomidx)
+                      if s_atom_symbol == ua_mol_H_s:
+                           atom_sametype.append(atomidx)
+                       # get same type atoms
+                      print("atom_sametype",atom_sametype)
+                   atom_lower1H=[]
+                   for idx in atom_sametype:
+                       tmp = get_linkage_hydrogen_num(mol, idx)
+                       if (tmp -1)== ua_mol_H:
+                           atom_lower1H.append(idx)
+                   if len(atom_lower1H)>1:
+                       atom_diff =[]
+                       for idx in atom_lower1H:
+                           tmp =abs(int(unsaturated_atom)-int(idx))
+                           atom_diff.append(tmp)
+                       atom_ua = atom_lower1H[atom_diff.index(min(atom_diff))]
+                   else:
+                       atom_ua =atom_lower1H[0]
+                   
+                   saturated_atoms.append(unsaturated_atom)
+                   unsaturated_atom =atom_ua
+                   saturated_atoms.remove(unsaturated_atom)
 
-                for idx in atom_sametype:
-                    tmp = get_linkage_hydrogen_num(mol, idx)
-                    if (tmp -1)== ua_mol_H:
-                        atom_lower1H=idx
-                        print(idx)
-                saturated_atoms.append(unsaturated_atom)
-                unsaturated_atom =atom_lower1H
-                saturated_atoms.remove(unsaturated_atom)
-        
+
             for k in saturated_atoms:
                 addHs = get_linkage_hydrogen_idx(mol,k)
-                for H in addHs:
-                    atomlists.append(H)
+            for H in addHs:
+                atomlists.append(H)
             unsaturated_Hs = get_linkage_hydrogen_idx(mol,int(unsaturated_atom))
             if len(unsaturated_Hs) != 0:
                 unsaturated_Hs.pop()
